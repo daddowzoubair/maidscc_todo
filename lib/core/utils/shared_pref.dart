05e3df147309c327d5d4b7511a7af82dc833023c
Zoubair Daddow <117348118+daddowzoubair@users.noreply.github.com>
2024-11-15 02:03:56 +0400

    create set/get userInfo functions

--- lib/core/utils/shared_pref.dart
+++ lib/core/utils/shared_pref.dart
@@ -1,6 +1,10 @@
 
+import 'dart:convert';
+
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../features/authentication/data/entities/user_info/user_info_model.dart';
 
 @LazySingleton(order: 1)
 class SharedPreferencesStorage {
@@ -9,6 +13,7 @@ class SharedPreferencesStorage {
 
   final String _accessToken = "ACCESS_TOKEN";
   final String _refreshToken = "REFRESH_TOKEN";
+  final String _userInfo = "USER_INFO";
 
  
   String get accessToken => _sharedPreferences.getString(_accessToken) ?? '';
@@ -22,6 +27,12 @@ class SharedPreferencesStorage {
 
   Future<bool> setRefreshToken(String refreshToken) async {
     return _sharedPreferences.setString(_refreshToken, refreshToken);
+  }
+
+  UserInfoModel get userInfo => UserInfoModel.fromJson(jsonDecode(_sharedPreferences.getString(_userInfo) ?? "{}"));
+
+  Future<bool> setUserInfo(UserInfoModel userInfo) async {
+    return _sharedPreferences.setString(_userInfo, jsonEncode(userInfo.toJson()));
   }
 
 }
--- lib/features/authentication/data/data_sources/local/authentication_local_data_source.dart
+++ lib/features/authentication/data/data_sources/local/authentication_local_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:injectable/injectable.dart';
 import '../../../../../core/utils/shared_pref.dart';
+import '../../entities/user_info/user_info_model.dart';
 
 @lazySingleton
 class AuthenticationLocalDataSource{
@@ -18,5 +19,12 @@ class AuthenticationLocalDataSource{
 
   Future<bool> setRefreshToken(String refreshToken) async {
     return localClient.setRefreshToken(refreshToken);
+  }
+
+
+  UserInfoModel get userInfo => localClient.userInfo;
+
+  Future<bool> setUserInfo(UserInfoModel userInfo) async {
+    return localClient.setUserInfo(userInfo);
   }
 }
