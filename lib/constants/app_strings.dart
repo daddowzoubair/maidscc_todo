3ced3f1f755caca3a335b37bb379fd1ce0209a7c
Zoubair Daddow <117348118+daddowzoubair@users.noreply.github.com>
2024-11-15 02:07:31 +0400

    add user profile Strings

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -1,3 +1,60 @@
 abstract class AppStrings {
   static const String generalErrorMessage = 'Oops, something went wrong';
+  static const String appTitle = 'Maids.cc Todos';
+  static const String authentication = 'Authentication';
+  static const String todoList = 'Todo List';
+
+  static const String profile = 'Profile';
+  static const String todos = 'Todos';
+
+  static const String userNameHint = 'Input your user name (Ex:emilys)';
+  static const String userNameLabel = 'Username';
+  static const String userNameValidation = 'Username is required';
+
+  static const String passwordHint = 'Input your passworde (Ex:emilyspass)';
+  static const String passwordLabel = 'Password';
+  static const String passwordValidation = 'Password is required';
+
+  static const String login = 'Login';
+  static const String error = 'Error';
+
+  static const String personalInformation = 'Personal Information';
+  static const String contactDetails = 'Contact Details';
+  static const String physicalAttributes = 'Physical Attributes';
+  static const String address = 'Address';
+  static const String companyInformation = 'Company Information';
+  static const String bankDetails = 'Bank Details';
+  static const String cryptoWallet = 'Crypto Wallet';
+  static const String education = 'Education';
+
+  static const String name = 'Name';
+  static const String maidenName = 'Maiden Name';
+  static const String username = 'Username';
+  static const String age = 'Age';
+  static const String gender = 'Gender';
+  static const String birthDate = 'Birth Date';
+  static const String role = 'Role';
+  static const String email = 'Email';
+  static const String phone = 'Phone';
+  static const String height = 'Height';
+  static const String heightUnit = 'cm';
+  static const String weight = 'Weight';
+  static const String weightUnit = 'kg';
+  static const String eyeColor = 'Eye Color';
+  static const String hair = 'Hair';
+  static const String bloodGroup = 'Blood Group';
+  static const String city = 'City';
+  static const String postalCode = 'Postal Code';
+  static const String country = 'Country';
+  static const String companyName = 'Company Name';
+  static const String title = 'Title';
+  static const String department = 'Department';
+  static const String officeAddress = 'Office Address';
+  static const String cardType = 'Card Type';
+  static const String cardNumber = 'Card Number';
+  static const String cardExpire = 'Card Expire';
+  static const String coin = 'Coin';
+  static const String walletAddress = 'Wallet Address';
+  static const String network = 'Network';
+  static const String university = 'University';
 }
